4d246c173398673e858f7083e29fba7c70c76b55
Ali Tykhomyrova <43432501+tykhomyrovaAlexandria@users.noreply.github.com>
2020-10-22 04:22:26 -0600

    trying to understand services

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {Recipe} from '../recipe.model';
+import { RecipeService } from '../recipe.service';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,21 +9,14 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.dkoding.in/wp-content/uploads/Gilmore-Girls-A-Year-In-The-Life-Trending-Today-DKODING-2.jpg'),
-    new Recipe('Another Test Recipe', 'This is simply a test', 'https://www.dkoding.in/wp-content/uploads/Gilmore-Girls-A-Year-In-The-Life-Trending-Today-DKODING-2.jpg')
-  ];
+  recipes: Recipe[];
 
   
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
-  }
-
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
+    this.recipes = this.recipeService.getRecipes();
   }
 
 }
